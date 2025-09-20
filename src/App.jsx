062411e3570691b9d9b9d6cc978a0cfecd05af50
MariamMahmoudMohammedIbrahem <mariam.mahmoud2811@gmail.com>
2025-09-20 20:46:00 +0300

    update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Project from "./pages/Project"
